8fddf45dd1565554918322e10107be858099b335
Vid <vid@uoregon.edu>
2016-05-24 23:18:24 -0700

    Replaced import w/ usable code from the boilerplate

--- index.ios.js
+++ index.ios.js
@@ -1,8 +1,11 @@
 // Impoart some code we need
-var React = require('react-native');
-var AppRegistry = React.AppRegistry;
-var Text = React.Text;
-var View = React.View;
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
 
 // Create a react component
 var Weekdays = React.createClass({
@@ -13,7 +16,7 @@ var Weekdays = React.createClass({
       </Text>
     </View>
   }
-})
+});
 
 // Show the react component on the screen
 AppRegistry.registerComponent('weekdays', function() {
