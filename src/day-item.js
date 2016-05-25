bdb492ab1e685bb08a2e00be3a98d52a6f763a9a
Vid <vid@uoregon.edu>
2016-05-24 23:46:47 -0700

    added content to day-item.js

--- src/day-item.js
+++ src/day-item.js
@@ -1,7 +1,18 @@
 // Import some code we need
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+} from 'react-native';
 
 // Create our component
-
+var DayItem = React.createClass({
+  render: function() {
+    return <Text>
+      A day of the week
+    </Text>
+  }
+});
 // Style our component
 
 // Make this code available elsewhere
