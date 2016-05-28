4198093ad61daf2ddc430c1e184a02079f0690a3
Vid <vid@uoregon.edu>
2016-05-27 23:39:42 -0700

    entered styles from tutorial

--- index.ios.js
+++ index.ios.js
@@ -37,11 +37,11 @@ var Stopwatch = React.createClass({
   },
   laps: function(){
     return this.state.laps.map(function(time, index){
-      return <View>
-        <Text>
+      return <View style={styles.lap}>
+        <Text style={styles.lapText}>
           Lap #{index + 1}
         </Text>
-        <Text>
+        <Text style={styles.lapText}>
           {formatTime(time)}
         </Text>
       </View>
@@ -136,6 +136,13 @@ var styles = StyleSheet.create({
   },
   stopButton: {
     borderColor: '#CC0000'
+  },
+  lap: {
+    justifyContent: 'space-around',
+    flexDirection: 'row'
+  },
+  lapText: {
+    fontSize: 30
   }
 });
 
