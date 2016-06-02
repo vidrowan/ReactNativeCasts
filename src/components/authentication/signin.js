e5545cccbd7e38cb513bfc320290c39b1c8b1250
Vid <vid@uoregon.edu>
2016-06-01 23:54:46 -0700

    Adding sign up button

--- src/components/authentication/signin.js
+++ src/components/authentication/signin.js
@@ -38,6 +38,7 @@ module.exports = React.createClass({
 
         <Text style={styles.label}>{this.state.errorMessage}</Text>
         <Button text={'Sign In'} onPress={this.onPress} />
+        <Button text={'I need an account...'} onPress={this.onSignupPress} />
       </View>
     );
   },
