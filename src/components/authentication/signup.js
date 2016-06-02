74253639a4ec0991c780c0b9fb7970254c25a057
Vid <vid@uoregon.edu>
2016-06-02 00:21:57 -0700

    added signup.js

---
+++ src/components/authentication/signup.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View
+} from 'react-native';
+
+
+var Parse = require('parse/react-native');
+var Button = require('../common/button');
+
+module.exports = React.createClass({
+  getInitialState: function() {
+    return {
+      username: '',
+      password: '',
+      passwordConfirmation: '',
+      errorMessage: ''
+    };
+  },
+  render: function() {
+    return (
+      <View style={styles.container}>
+        <Text>Sign Up</Text>
+
+        <Text style={styles.label}>Username:</Text>
+        <TextInput
+          value={this.state.username}
+          onChangeText={(text) => this.setState({username: text})}
+          style={styles.input} />
+
+        <Text style={styles.label}>Password:</Text>
+        <TextInput
+          secureTextEntry={true}
+          value={this.state.password}
+          onChangeText={(text) => this.setState({password: text})}
+          style={styles.input} />
+
+        <Text style={styles.label}>Confirm Password:</Text>
+        <TextInput
+          secureTextEntry={true}
+          value={this.state.passwordConfirmation}
+          onChangeText={(text) => this.setState({passwordConfirmation: text})}
+          style={styles.input} />
+
+        <Text style={styles.label}>{this.state.errorMessage}</Text>
+        <Button text={'Signup'} onPress={this.onSignUpPress} />
+        <Button text={'I have an account...'} onPress={this.onSigninPress} />
+      </View>
+    );
+  },
+  onSignUpPress: function() {
+    if (this.state.password !== this.state.passwordConfirmation ) {
+      return this.setState({errorMessage: 'Your passwords do not match'});
+    }
+
+    var user = new Parse.User();
+    user.set('username', this.state.username);
+    user.set('password', this.state.password);
+
+    user.signUp(null, {
+      success: (user) => { console.log(user); },
+      error: (user, error) => { console.log(error); }
+    });
+  },
+  onSigninPress: function() {
+    this.props.navigator.pop();
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white'
+  },
+  label: {
+    fontSize: 18
+  },
+  input: {
+    padding: 4,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    margin: 5,
+    width: 200,
+    alignSelf: 'center'
+  }
+});
