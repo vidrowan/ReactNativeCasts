b4d651790ab1a54f30bb5e366ee600e359367bc2
Vid <vid@uoregon.edu>
2016-06-01 23:06:25 -0700

    adding sign in fields and styling. Man you have to style fields from scratch

--- src/components/authentication/signin.js
+++ src/components/authentication/signin.js
@@ -10,7 +10,11 @@ module.exports = React.createClass({
   render: function() {
     return (
       <View style={styles.container}>
-        <Text>Sign In, Yo!</Text>
+        <Text>Sign In</Text>
+        <Text style={styles.label}>Username:</Text>
+        <TextInput style={styles.input} />
+        <Text style={styles.label}>Password:</Text>
+        <TextInput secureTextEntry={true} style={styles.input} />
       </View>
     );
   }
@@ -21,5 +25,18 @@ var styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  input: {
+    padding: 4,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    margin: 5,
+    width: 200,
+    alignSelf: 'center'
+  },
+  label: {
+    fontSize: 18
   }
 });
